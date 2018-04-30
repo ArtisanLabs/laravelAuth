a97b820f99044c06b403f04b71368775bba02058
Artisan <artisan254@gmail.com>
2018-04-30 09:41:54 +0300

    Added the admins guard for multiple auth

--- config/auth.php
+++ config/auth.php
@@ -41,6 +41,11 @@ return [
             'provider' => 'users',
         ],
 
+	    'admin' => [
+            'driver' => 'session',
+            'provider' => 'admins',
+        ],
+
         'api' => [
             'driver' => 'token',
             'provider' => 'users',
@@ -70,7 +75,12 @@ return [
             'model' => App\User::class,
         ],
 
-        // 'users' => [
+	    'admins' => [
+		    'driver' => 'eloquent',
+		    'model' => App\Admin::class,
+	    ],
+
+	    // 'users' => [
         //     'driver' => 'database',
         //     'table' => 'users',
         // ],
