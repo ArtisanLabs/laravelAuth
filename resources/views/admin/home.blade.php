07f097685330303049e24ac74af2c4dc252db784
Artisan <artisan254@gmail.com>
2018-04-30 09:35:35 +0300

    Changed the min password character length to 8

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: artisan
+ * Date: 4/30/18
+ * Time: 9:18 AM
+ */
---
+++ resources/views/auth/admin/login.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: artisan
+ * Date: 4/29/18
+ * Time: 12:34 PM
+ */
